9a3cd57a3e71d8ddd850fcdbb4b0d1270ffe8865
Your Name <you@example.com>
2019-05-05 05:58:03 -0700

    socket url fix

--- client/src/Store/Actions/DataCenter/PastEventsActions/socketActions/pastEventProgress.js
+++ client/src/Store/Actions/DataCenter/PastEventsActions/socketActions/pastEventProgress.js
@@ -1,6 +1,6 @@
 import { FIGHTER_LIST_PROGRESS, COLLECT_PAST_EVENT_FIGHTERS } from '../../actionTypes';
 import io from 'socket.io-client';
-let socket = io.connect( 'localhost:3001/' );
+let socket = io.connect( 'lalgo-x-beta.herokuapp.com' );
 
 export const emitCollectFighters = ( url, dispatch ) => {
     if( url ) {
