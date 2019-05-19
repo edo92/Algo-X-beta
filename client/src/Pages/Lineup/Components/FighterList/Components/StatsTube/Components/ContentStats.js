c9da39160d0848345aae2ed36ed76684511df8a2
Your Name <you@example.com>
2019-05-19 14:01:59 -0700

    check

--- client/src/Pages/Lineup/Components/FighterList/Components/StatsTube/Components/ContentStats.js
+++ client/src/Pages/Lineup/Components/FighterList/Components/StatsTube/Components/ContentStats.js
@@ -1,40 +1,36 @@
 import React from 'react';
+import './CSS/ContentStats.css';
 
 const ContentStats = props => {
+    let { basic, record, careerStats } = props.stats;
+
     return(
         <div className='col-12 p-0'>
-                <ul className='row col-12 p-0 m-0 list-none'>
-                <li className='col-12 p-0'>
-                            <p>
-                                <span>record</span>
-                                <span>
-                                    <span>
-                                        <span>Win</span>
-                                        <span>{props.stats.record.w}</span>
-                                    </span>
-                                    <span>
-                                        <span>Lose</span>
-                                        <span>{props.stats.record.l}</span>
-                                    </span>
-                                </span>
-                                <span>{props.stats.record.w}</span>
-                                <span>{props.stats.record.w}</span>
-                            </p>
-                    </li>
-                    <li className='col-12 p-0'>
-                        <ul className='p-0 m-0'>
-                            <li className='col-12 p-0'>
-                                <p>{`age:  ${props.stats.basic.age}`}</p>
-                                <p>{`height:  ${props.stats.basic.height}`}</p>
-                                <p>{`reach:  ${props.stats.basic.reach}`}</p>
-                                <p>{`weight:  ${props.stats.basic.weight}`}</p>
-                                <p>{`stance:  ${props.stats.basic.stance.stance}`}</p>
-                            </li>
-                        </ul>
-                    </li>
-                    
-                </ul>
+            <div className='col-12 p-0 text-center'>
+                <p><span>record: </span>
+                <span>
+                    {`${record.w}-${record.l}-${record.nc}`}
+                </span></p>                
             </div>
+            <div id='fighter-stats' className='col-12 p-0'>
+                <div className='col-6 p-0'>
+                    <p>{`age:  ${props.stats.basic.age}`}</p>
+                    <p>{`height:  ${props.stats.basic.height}`}</p>
+                    <p>{`reach:  ${props.stats.basic.reach}`}</p>
+                    <p>{`weight:  ${props.stats.basic.weight}`}</p>
+                    <p>{`stance:  ${props.stats.basic.stance.stance}`}</p>
+                </div>
+                <div className='col-6 p-0'>
+                    <p>{`sapm:  ${ careerStats.sapm }`}</p>
+                    <p>{`slpm:  ${props.stats.basic.slpm}`}</p>
+                    <p>{`strAcc:  ${props.stats.basic.strAcc}`}</p>
+                    <p>{`strDef:  ${props.stats.basic.strDef}`}</p>
+                    <p>{`tdAcc:  ${props.stats.basic.tdAcc}`}</p>
+                    <p>{`tdAvg:  ${props.stats.basic.tdAvg}`}</p>
+                    <p>{`tdDef:  ${props.stats.basic.tdDef}`}</p>
+                </div>
+            </div>             
+        </div>
     )
 }
 
