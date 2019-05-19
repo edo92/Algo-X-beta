b88edfa25f92b70589c4f96637f839fc4c4b1514
Your Name <you@example.com>
2019-05-19 13:43:20 -0700

    check

--- client/src/Pages/Lineup/Components/FighterList/Components/StatsTube/Components/ContentStats.js
+++ client/src/Pages/Lineup/Components/FighterList/Components/StatsTube/Components/ContentStats.js
@@ -3,14 +3,36 @@ import React from 'react';
 const ContentStats = props => {
     return(
         <div className='col-12 p-0'>
-                <ul className='col-12 p-0 m-0 list-none'>
-                    <li>
-                        <p>{`age:  ${props.stats.basic.age}`}</p>
-                        <p>{`height:  ${props.stats.basic.height}`}</p>
-                        <p>{`reach:  ${props.stats.basic.reach}`}</p>
-                        <p>{`weight:  ${props.stats.basic.weight}`}</p>
-                        <p>{`stance:  ${props.stats.basic.stance.stance}`}</p>
+                <ul className='row col-12 p-0 m-0 list-none'>
+                <li className='col-12 p-0'>
+                            <p>
+                                <span>record</span>
+                                <span>
+                                    <span>
+                                        <span>Win</span>
+                                        <span>{props.stats.record.w}</span>
+                                    </span>
+                                    <span>
+                                        <span>Lose</span>
+                                        <span>{props.stats.record.l}</span>
+                                    </span>
+                                </span>
+                                <span>{props.stats.record.w}</span>
+                                <span>{props.stats.record.w}</span>
+                            </p>
                     </li>
+                    <li className='col-12 p-0'>
+                        <ul className='p-0 m-0'>
+                            <li className='col-12 p-0'>
+                                <p>{`age:  ${props.stats.basic.age}`}</p>
+                                <p>{`height:  ${props.stats.basic.height}`}</p>
+                                <p>{`reach:  ${props.stats.basic.reach}`}</p>
+                                <p>{`weight:  ${props.stats.basic.weight}`}</p>
+                                <p>{`stance:  ${props.stats.basic.stance.stance}`}</p>
+                            </li>
+                        </ul>
+                    </li>
+                    
                 </ul>
             </div>
     )
