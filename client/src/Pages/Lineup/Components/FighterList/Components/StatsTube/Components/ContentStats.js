2bdf3e9eafd05e7e8cac3a29b120126cca0016a9
Your Name <you@example.com>
2019-05-19 13:32:55 -0700

    quick stats tube

--- client/src/Pages/Lineup/Components/FighterList/Components/StatsTube/Components/ContentStats.js
+++ client/src/Pages/Lineup/Components/FighterList/Components/StatsTube/Components/ContentStats.js
@@ -1,21 +1,16 @@
 import React from 'react';
 
 const ContentStats = props => {
-    console.log('imp checkgn stats ----', props.stats )
-    const BasicStats = stats => {
-        for( let key in stats ){
-            return(
-                <li className='col-12 p-0'>
-                    <p>{`${key}: ${stats[key]}`}</p>
-                </li>  
-            )
-        }
-    };
-
     return(
         <div className='col-12 p-0'>
                 <ul className='col-12 p-0 m-0 list-none'>
-                    <BasicStats stats={ props.stats.basic }/>
+                    <li>
+                        <p>{`age:  ${props.stats.basic.age}`}</p>
+                        <p>{`height:  ${props.stats.basic.height}`}</p>
+                        <p>{`reach:  ${props.stats.basic.reach}`}</p>
+                        <p>{`weight:  ${props.stats.basic.weight}`}</p>
+                        <p>{`stance:  ${props.stats.basic.stance.stance}`}</p>
+                    </li>
                 </ul>
             </div>
     )
